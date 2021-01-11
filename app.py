4a75faf3b372dc5850652c643f75ae457c0c7215
Santiago LD <sldenazis@gmail.com>
2021-01-11 15:50:26 -0300

    added initial version

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from prometheus_client import make_wsgi_app
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from werkzeug.serving import run_simple
+from flask_prometheus_metrics import register_metrics
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello!"
+
+# provide app's version and deploy environment/config name to set a gauge metric
+register_metrics(app, app_version="v0.1.2", app_config="staging")
+
+# Plug metrics WSGI app to your main app with dispatcher
+dispatcher = DispatcherMiddleware(app.wsgi_app, {"/metrics": make_wsgi_app()})
+
+run_simple(hostname="0.0.0.0", port=5000, application=dispatcher)
